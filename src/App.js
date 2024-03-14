c4031617049964f1e5cd8852df0ecfb3eaf35f76
Lightlana888 <svetlana.yakuta@gmail.com>
2024-03-14 15:41:53 +0100

    React-start-studying

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { Chart } from "react-google-charts";
+
+export const data = [
+  ["Task", "Hours per Day"],
+  ["Work", 9],
+  ["Eat", 3],
+  ["Commute", 2],
+  ["Beuty routine", 1],
+  ["Sleep", 8],
+  ["Yoga", 1]
+];
+
+export const options = {
+  title: "My Daily Activities",
+  is3D: true,
+  backgroundColor: '#282c34',
+  titleTextStyle: { color: 'white', fontSize: 24 },
+  legendTextStyle: { color: 'white' },
+};
 
 function App() {
   return (
@@ -7,16 +26,17 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello React!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Chart
+          chartType="PieChart"
+          data={data}
+          options={options}
+          width={"100%"}
+          height={"500px"}
+
+
+        />
       </header>
     </div>
   );
